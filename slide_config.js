a637377c33024914a4e8a282c9d67c6fdf57dcbb
Krister Kari <krister.kari@webtehdas.fi>
2013-05-16 23:22:54 +0300

    slides

---
+++ slide_config.js
@@ -0,0 +1,27 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Building Angry Birds Toons<br>Channel Web App',
+    subtitle: '',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    favIcon: 'images/google_developers_logo_tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ]
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Krister Kari',
+    company: 'Web developer, Rovio',
+    gplus: '',
+    twitter: '@kristerkari',
+    www: '',
+    github: 'http://github.com/kristerkari'
+  }]
+};
+
